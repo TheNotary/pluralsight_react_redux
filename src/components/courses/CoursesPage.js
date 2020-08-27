7754735219b4203e736cbecef639ff25c183d4d7
TheNotary <no@email.plz>
2020-08-27 15:25:48 -0500

    completes 9-3

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+    state = {
+        course: {
+            title: ""
+        }
+    };
+
+    handleChange = (event) => {
+        const course = { ...this.state.course, title: event.target.value };
+        this.setState({ course: course });
+    };
+
     render() {
-        return <h2>Courses</h2>
+        return (
+            <form>
+                <h2>Courses</h2>
+                <h3>Add course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="save" />
+            </form>
+        )
     }
 }
 
