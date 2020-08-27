fda9153d648198cf77854ba981dead3c6c0552a3
TheNotary <no@email.plz>
2020-08-27 09:09:34 -0500

    completes 5-4

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Pluralsight admin</h1>
+    <p>React, redux...</p>
+    <Link to="about" className="btn btn-primary btn-lg">
+      Learn more
+    </Link>
+  </div>
+);
+
+export default HomePage;
