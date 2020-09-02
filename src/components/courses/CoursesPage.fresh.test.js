dad76e6cc83c72aeecaf5c1ae48fe616dae9e8c5
TheNotary <no@email.plz>
2020-09-02 15:59:03 -0500

    this is really hard :(

--- src/components/courses/CoursesPage.fresh.test.js
+++ src/components/courses/CoursesPage.fresh.test.js
@@ -1,6 +1,5 @@
+// __tests__/fetch.test.js
 import React from "react";
-import CoursesPage from "./CoursesPage";
-import renderer from "react-test-renderer";
 import { courses, authors } from "../../../tools/mockData";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
@@ -8,45 +7,59 @@ import fetchMock from "fetch-mock";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 
-import * as courseActions from "../../redux/actions/courseActions";
-
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
+import CoursesPage from "./CoursesPage";
+
+const server = setupServer(
+  rest.get("/greeting", (req, res, ctx) => {
+    return res(ctx.json({ greeting: "hello there" }));
+  })
+);
+
 describe("CoursesPage", () => {
-  afterEach(() => {
-    fetchMock.restore(); // this re-inits fetchMock, which is a stateful testing entity
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  test("loads and displays greeting", async () => {
+    fetchMock.mock("*", {
+      body: courses,
+      headers: { "content-type": "application/json" },
+    });
+
+    const props = {
+      // actions: {},
+      courses,
+      authors,
+      loading: false,
+      history: {},
+      match: {},
+    };
+
+    const middleware = [thunk];
+    const mockStore = configureMockStore(middleware);
+    const store = mockStore({ courses: courses, authors: authors }); // define the initial state of the store
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <CoursesPage {...props} />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getAllByText("Delete").length).toBe(10);
+    fireEvent.click(screen.getAllByText("Delete")[0]);
+
+    // await waitFor(() => screen.getByText("Course deleted"));
+    // screen.debug();
+    expect(screen.getAllByText("Delete").length).toBe(9);
+
+    // expect(screen.getByRole("heading")).toHaveTextContent("hello there");
+    // expect(screen.getByRole("button")).toHaveAttribute("disabled");
   });
-
-  it("works", () => {
-    expect(true).toBe(true);
-  });
-
-  // it("does a meaningful test...", () => {
-  //   fetchMock.mock("*", {
-  //     body: courses,
-  //     headers: { "content-type": "application/json" },
-  //   });
-
-  //   const tree = render();
-
-  //   expect(tree).toMatchSnapshot();
-
-  //   const startingDeleteButtons = tree.root.findAllByProps({
-  //     className: "btn btn-outline-danger delete-course-btn",
-  //   });
-  //   expect(startingDeleteButtons.length).toBe(10);
-
-  //   renderer.act(startingDeleteButtons[0].props.onClick);
-  //   renderer.act(courseActions.deleteCourse, courses[0]);
-  //   // courseActions.deleteCourse(courses[0]);
-
-  //   // the onClick handler should have fired... but it doesn't I guess
-
-  //   const endingDeleteButtons = tree.root.findAllByProps({
-  //     className: "btn btn-outline-danger delete-course-btn",
-  //   });
-
-  //   expect(endingDeleteButtons.length).toBe(9);
-  // });
 });
